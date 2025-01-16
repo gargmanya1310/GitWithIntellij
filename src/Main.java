c0b1866eebc5241a0423648b19f3b1e6a47fb5ed
gargmanya1310 <gargmanya1310@gmail.com>
2025-01-16 12:54:45 +0530

    Stream API commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.SQLOutput;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -23,36 +24,42 @@ public class Main
     public static void main(String[] args)
     {
         // Lambda expression with zero parameters
-        ZeroParameter zeroParamLambda = ()-> System.out.println("This is a zero-parameter lambda expression!");
-        zeroParamLambda.display();
-
-        // lambda expression to implement above functional interface. This interface by default implements abstractFun()
-        FuncInterface fobj = (int x)->System.out.println(2*x);
-        fobj.abstractFun(5);
-
-        //simple lambda expressions
+//        ZeroParameter zeroParamLambda = ()-> System.out.println("This is a zero-parameter lambda expression!");
+//        zeroParamLambda.display();
+//
+//        // lambda expression to implement above functional interface. This interface by default implements abstractFun()
+//        FuncInterface fobj = (int x)->System.out.println(2*x);
+//        fobj.abstractFun(5);
+//
+//        //simple lambda expressions
         ArrayList<Integer> arrL = new ArrayList<Integer>();
-        arrL.add(1);
+        arrL.add(7);
         arrL.add(2);
-        arrL.add(3);
+        arrL.add(8);
         arrL.add(4);
+//
+//        // Using lambda expression to print all elements
+//        // of arrL
+//
+//        System.out.println("Elements of the ArrayList : ");
+//        arrL.forEach(n -> System.out.println(n));
+//
+//        // Using lambda expression to print even elements
+//        // of arrL
+//        System.out.println(
+//                "Even elements of the ArrayList : ");
+//        arrL.forEach(n -> {
+//            if (n % 2 == 0)
+//                System.out.println(n);
+//        });
 
-        // Using lambda expression to print all elements
-        // of arrL
+        //Stream API
+        Stream<Integer> data = arrL.stream();
+        Stream<Integer> sortedData=data.sorted();
+        sortedData.forEach(n->System.out.println(n));
+        //arrL.forEach(n->System.out.println(n));g
 
-        System.out.println("Elements of the ArrayList : ");
-        arrL.forEach(n -> System.out.println(n));
 
-        // Using lambda expression to print even elements
-        // of arrL
-        System.out.println(
-                "Even elements of the ArrayList : ");
-        arrL.forEach(n -> {
-            if (n % 2 == 0)
-                System.out.println(n);
-        });
-
-        //String API
 
 
     }
