b434c5f7438340968f011911713fe61dd862fd2b
gargmanya1310 <gargmanya1310@gmail.com>
2025-01-17 12:10:24 +0530

    Test private interface methods

--- Java 9/src/Main.java
+++ Java 9/src/Main.java
@@ -3,19 +3,35 @@
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
-public class Main {
-    public static void main(String[] args)throws FileNotFoundException
+interface Sayable{
+    default void say() {
+        saySomething();
+    }
+    // Private method inside interface
+    private void saySomething() {
+        System.out.println("Hello... I'm private method");
+    }
+}
+
+public class Main implements Sayable
+{
+    public static void main(String[] args)//throws FileNotFoundException
     {
 
 //try-with-resources
-        FileOutputStream fileStream=new FileOutputStream("javatpoint.txt");
-        try(fileStream){
-            String greeting = "Welcome to javaTpoint.";
-            byte b[] = greeting.getBytes();
-            fileStream.write(b);
-            System.out.println("File written");
-        }catch(Exception e) {
-            System.out.println(e);
-        }
+//        FileOutputStream fileStream=new FileOutputStream("javatpoint.txt");
+//        try(fileStream){
+//            String greeting = "Welcome to javaTpoint.";
+//            byte b[] = greeting.getBytes();
+//            fileStream.write(b);
+//            System.out.println("File written");
+//        }catch(Exception e) {
+//            System.out.println(e); }
+
+// private interface methods
+        Sayable s = new Main();
+        s.say();
+
+
     }
 }
